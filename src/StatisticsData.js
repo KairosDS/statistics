0a46114121ec52397e993fa74f8d15115f116f39
Kairos <jjgiraldezjurado@gmail.com>
2021-03-26 14:20:46 +0100

    caustics when the component is out of viewport and scrolling, when it appears in viewport and the animated attribute is true it activates stats movement such as counters

--- src/StatisticsData.js
+++ src/StatisticsData.js
@@ -54,7 +54,7 @@ export class StatisticsData extends HTMLChildrenMixin(LitElement) {
     super.connectedCallback();
     this.statistics = this._HTMLChildren();
     window.onload = () => {
-      if(this.animation) {this.animatedStats();}
+      if(this.animation) { this.loadAnimation() }
       if(this.scrollPage) { window.addEventListener('scroll',this.getPageScroll);}
     }
     
@@ -71,14 +71,77 @@ export class StatisticsData extends HTMLChildrenMixin(LitElement) {
     }
   }
 
+
+
+  
   addAnimationDelay(item, i) {
     setTimeout(() => {
       item.classList.add('statistics__animate');
     }, i * 200);
-    if(this.scrollPage || this.animation && this.scrollPage) {this.animatedStats();
-    window.removeEventListener('scroll', this.getPageScroll);}
+    if(this.scrollPage || this.animation && this.scrollPage) {
+      if(this.animation){this.loadAnimation()}
+      window.removeEventListener('scroll', this.getPageScroll);}
   }
 
+  loadAnimation () {
+    const animationOnFocus = this.shadowRoot.querySelector('.statistics');
+    const stats = this.shadowRoot.querySelectorAll('#number');
+    function animatedStats() {
+      
+      stats.forEach((stat) => {
+        const patt = /(\D+)?(\d+)(\D+)?(\d+)?(\D+)?/;
+        let result = [...patt.exec(stat.getAttribute('data-target'))];
+        result.shift();
+        result = result.filter((res) => res != null);
+        while (stat.firstChild) {
+          stat.removeChild(stat.firstChild);
+        }
+        for (const res of result) {
+          if (isNaN(res)) {
+            stat.insertAdjacentHTML('beforeend', `<span>${res}</span>`);
+          } else {
+            for (let i = 0; i < res.length; i++) {
+              stat.insertAdjacentHTML(
+                'beforeend',
+                `<span data-value="${res[i]}"><span>&ndash;</span>${Array(parseInt(res[i]) + 1).join(0).split(0).map((x, j) => `
+                  <span>${j}</span>`)
+                  .join('')}</span>`,
+              );
+            }
+          }
+        }
+  
+        const ticks = [...stat.querySelectorAll('span[data-value]')];
+  
+        const activate = () => {
+          setTimeout(() => {
+            for (const tick of ticks) {
+              const dist = parseInt(tick.getAttribute('data-value')) + 1;
+              tick.style.transform = `translateY(-${dist * 100}%)`;
+            }
+          }, 500);
+        };
+        activate();
+      });
+     }
+  
+    const options = {
+      threshold: 0.8,
+    };
+    /* eslint-disable-next-line */
+    function callBack(entries, observer) {
+      if (entries[0].isIntersecting) {
+        animatedStats();
+        observer.unobserve(entries[0].target);
+      }
+    }
+  
+    const observerImages = new IntersectionObserver(callBack, options);
+    observerImages.observe(animationOnFocus);
+
+    
+   
+  }
 
 
   animatedStats() {
@@ -91,13 +154,10 @@ export class StatisticsData extends HTMLChildrenMixin(LitElement) {
       while (stat.firstChild) {
         stat.removeChild(stat.firstChild);
       }
-      // eslint-disable-next-line no-restricted-syntax
       for (const res of result) {
-        // eslint-disable-next-line no-restricted-globals
         if (isNaN(res)) {
           stat.insertAdjacentHTML('beforeend', `<span>${res}</span>`);
         } else {
-          // eslint-disable-next-line no-restricted-syntax
           for (let i = 0; i < res.length; i++) {
             stat.insertAdjacentHTML(
               'beforeend',
@@ -113,9 +173,7 @@ export class StatisticsData extends HTMLChildrenMixin(LitElement) {
 
       const activate = () => {
         setTimeout(() => {
-          // eslint-disable-next-line no-restricted-syntax
           for (const tick of ticks) {
-            // eslint-disable-next-line radix
             const dist = parseInt(tick.getAttribute('data-value')) + 1;
             tick.style.transform = `translateY(-${dist * 100}%)`;
           }
@@ -133,7 +191,7 @@ export class StatisticsData extends HTMLChildrenMixin(LitElement) {
                   ${this.statistics.map((statistic) => html`
                     <div class="statistics__content">
                       <p id="number" class="statistics__content-number" 
-                      part="stats-number" data-target="${statistic.number}">${this.animation ? null : statistic.number}</p>
+                      part="stats-number" data-target="${statistic.number}">${statistic.number}</p>
                       <p class="statistics__content-text" part="stats-text"><span>${statistic.infoTitle}</span>${statistic.description}</p>
                     </div>
                   `)}
